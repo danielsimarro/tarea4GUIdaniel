c1754606bd1c1936c34cede270b5bfbb7e3d7eb7
daniel <danisimarroreigada@gmail.com>
2021-03-23 20:16:16 +0100

    Creación de la clase PanelPrincipal (Ejemplo)

---
+++ src/main/java/calculadora/PanelPrincipal.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author NitroPc
+ */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+/**
+ * @author Juan Carlos Fernández Vico
+ */
+public class PanelPrincipal extends JPanel {
+
+    // Atributos de la clase (privados)
+    private PanelBotones botonera;
+    private JTextArea areaTexto;
+    private int tipoOperacion;
+
+    // Constructor
+    public PanelPrincipal() {
+        initComponents();
+        tipoOperacion = -1; // No hay operaciones en la calculadora
+    }
+
+    // Se inicializan los componentes gráficos y se colocan en el panel
+    private void initComponents() {
+        // Creamos el panel de botones
+        botonera = new PanelBotones();
+        // Creamos el área de texto
+        areaTexto = new JTextArea(10, 50);
+        areaTexto.setEditable(false);
+        areaTexto.setBackground(Color.white);
+
+        //Establecemos layout del panel principal
+        this.setLayout(new BorderLayout());
+        // Colocamos la botonera y el área texto
+        this.add(areaTexto, BorderLayout.NORTH);
+        this.add(botonera, BorderLayout.SOUTH);
+
+    }
+}
