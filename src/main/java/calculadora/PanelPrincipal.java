dfa480293aa90fd92e806669b5a41ac5ef536afb
daniel <danisimarroreigada@gmail.com>
2021-03-24 14:28:58 +0100

    Parte hecha en clase

--- src/main/java/calculadora/PanelPrincipal.java
+++ src/main/java/calculadora/PanelPrincipal.java
@@ -48,7 +48,7 @@ public class PanelPrincipal extends JPanel implements ActionListener {
         this.add(areaTexto, BorderLayout.NORTH);
         this.add(botonera, BorderLayout.SOUTH);
 
-        for (JButton boton: this.botonera.getgrupoBotones()){
+        for (JButton boton : this.botonera.getgrupoBotones()) {
             boton.addActionListener(this);
         }
 
@@ -58,14 +58,88 @@ public class PanelPrincipal extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent ae) {
         // Se obtiene el objeto que desencadena el evento
         Object o = ae.getSource();
-        // Si es un botón
+        // Si es un botón 
         if (o instanceof JButton) {
-            System.out.println(((JButton) o).getText());
+//            System.out.println(((JButton) o).getText());
+            JButton boton = (JButton) o;
             areaTexto.setText(((JButton) o).getText());
+
+            String resultado = "";
+
+            switch (boton.getText()) {
+                case "0":
+                    resultado+="0";
+                    break;
+                case "1":
+                    resultado+="1";
+                    break;
+                case "2":
+                    resultado+="2";
+                    break;
+                case "3":
+                    resultado+="3";
+                    break;
+                case "4":
+                    resultado+="4";
+                    break;
+                case "5":
+                    resultado+="5";
+                    break;
+                case "6":
+                    resultado+="6";
+                    break;
+                case "7":
+                    resultado+="7";
+                    break;
+                case "8":
+                    resultado+="8";
+                    break;
+                case "9":
+                    resultado+="9";
+                    break;
+                case "+":
+                    resultado+="+";
+                    break;
+                case "-":
+                    resultado+="-";
+                    break;
+                case "/":
+                    resultado+="/";
+                    break;
+                case "*":
+                    resultado+="*";
+                    break;
+                case "=":
+                    
+                areaTexto.setText(resultado);
+                    
+                    break;
+                case "C":
+                    resultado+="C";
+                    resultado = "0";
+                    break;
+            }
+            
+            String ultimoCaracter = "";
+            String operador = "";
+            
+            if(resultado.length()== 1){
+                ultimoCaracter = resultado;
+            }else{
+                ultimoCaracter=resultado.substring(resultado.length()-1);
+            }
+            
+            if(ultimoCaracter.equals("+")|| ultimoCaracter.equals("-")||ultimoCaracter.equals("/")||
+                    ultimoCaracter.equals("*")){
+                for(int i =10; i<14;i++){
+                    this.botonera.grupoBotones[i].setEnabled(false);
+                    operador = ultimoCaracter;
+                }
+            }
         }
 
         // RESTO DEL CÓDIGO DE LA LÓGICA DE LA CALCULADORA
+        // Desabilitar botones setEnable (false)
     }
 
 }
-
